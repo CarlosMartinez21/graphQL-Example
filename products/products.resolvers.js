017e242cb902c535de21809bba2ec4e399615d88
CarlosMartinez21 <cmartinez2123@gmail.com>
2022-12-15 11:03:44 -0800

    added model and resolvers

---
+++ products/products.resolvers.js
@@ -0,0 +1,29 @@
+const {
+  getAllProducts,
+  getProductsByPrice,
+  getProductById,
+  addNewProduct,
+  addNewProductReview,
+} = require("./products.model");
+
+module.exports = {
+  Query: {
+    products: () => {
+      return getAllProducts();
+    },
+    productsByPrice: (_, args) => {
+      return getProductsByPrice(args.min, args.max);
+    },
+    productById: (_, args) => {
+      return getProductById(args.id);
+    },
+  },
+  Mutation: {
+    addNewProduct: (_, args) => {
+      return addNewProduct(args.id, args.description, args.price);
+    },
+    addNewProductReview: (_, args) => {
+      return addNewProductReview(args.id, args.rating, args.comment);
+    },
+  },
+};
--- server.js
+++ server.js
@@ -1,67 +1,17 @@
 const express = require("express");
-const { buildSchema } = require("graphql");
+const path = require("path");
 const { graphqlHTTP } = require("express-graphql");
 
-const schema = buildSchema(`
-    type Query {
-        products: [Product]
-        orders: [Order]
-    }
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-    type Product {
-        id: ID!
-        description: String!
-        price: Float!
-        reviews: [Review]
-    }
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-    type Review {
-        rating: Int!
-        comment: String
-    }
-
-    type Order {
-        date: String!
-        subtotal: Float!
-        items: [OrderItem]
-    }
-
-    type OrderItem {
-        product: Product!
-        quantity: Int!
-    }
-`);
-
-const root = {
-  products: [
-    {
-      id: "redshoe",
-      description: "Red Shoe",
-      price: 42.12,
-    },
-    {
-      id: "bluejean",
-      description: "Blue Jeans",
-      price: 55.55,
-    },
-  ],
-  orders: [
-    {
-      date: "2005-05-05",
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: "redshoe",
-            description: "Old Red Shoe",
-            price: 45.11,
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ],
-};
+const schema = makeExecutableSchema({
+  typeDefs: [typesArray],
+  resolvers: resolversArray,
+});
 
 const app = express();
 
@@ -69,7 +19,6 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
